bcff3d4ca3033310981d9fbe340094180bbb47b9
basuabhirup <basu.abhirup@yahoo.com>
2021-08-27 21:46:59 +0530

    Set takePhoto function to capture snapshots from the video and download.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+// Get video from user's webcam
 function getVideo() {
   navigator.mediaDevices.getUserMedia({
     video: true,
@@ -18,6 +19,7 @@ function getVideo() {
   })
 }
 
+// Render video to the canvas
 function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -30,4 +32,21 @@ function paintToCanvas() {
   }, 100)
 }
 
+// Take snapshot from the video
+function takePhoto() {
+  // Play shutter sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //Take data out of the paintToCanvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src=${data} alt="Handsome Boy">`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
